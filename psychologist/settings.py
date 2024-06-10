5b0d38d6124d1265f7385ecee57e07516a8c7a0b
Vladislav Belousov <vladbelousov@yahoo.com>
2024-06-10 14:26:29 +0100

    Trust localhost

--- psychologist/settings.py
+++ psychologist/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['vladbelousov.pythonanywhere.com', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://vladbelousov.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://vladbelousov.pythonanywhere.com', 'http://localhost/']
 
 
 # Application definition
