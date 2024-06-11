f78eb9607fabfdf47f6183c91b31f15a59ef37c9
Vladislav Belousov <vladbelousov@yahoo.com>
2024-06-11 18:47:35 +0100

    Update Review representation

--- reviews/models.py
+++ reviews/models.py
@@ -5,3 +5,6 @@ class Review(models.Model):
     review_text = models.TextField()
     author_name = models.CharField(max_length=100)
     pub_date = models.DateField("Date published")
+
+    def __str__(self):
+        return f"{self.author_name} - {self.review_text}"
